dbad7bcd32fb443697da84d15b06dca5d1d0e625
Alex Telenkov <ru3aah@gmail.com>
2023-08-02 18:02:48 +0300

    add checkstyle config

---
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+        assertEquals(1, 1);
+    }
+
+}
