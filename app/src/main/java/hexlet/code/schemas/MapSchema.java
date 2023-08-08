714d5430b33650a58baee541a855fec80a354b71
Alex Telenkov <ru3aah@gmail.com>
2023-08-08 22:13:12 +0300

    add Shape methods for complicated Maps

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -22,4 +22,25 @@ public class MapSchema extends CommonSchema {
                 && ((Map<?, ?>) map).size() == givenSize);
         return this;
     }
+    /**
+     * validates data inside Map.
+     * @param map Map
+     * @return MapSchema
+     */
+    public final MapSchema shape(final Map<String, CommonSchema> map) {
+        addRequirement(newMap
+                -> chekingMap((Map<String, CommonSchema>) newMap, map));
+        return this;
+    }
+
+    private boolean chekingMap(final Map<String, CommonSchema> orig,
+                               final Map<String, CommonSchema> shapedMap) {
+        for (Map.Entry<String, CommonSchema> map : shapedMap.entrySet()) {
+            String key = map.getKey();
+            if (!map.getValue().isValid(orig.get(key))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
